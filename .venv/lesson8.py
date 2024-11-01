2c572654868c4df7e92212cab58b93a6dd13c9ff
Python_basics <mburuhannah.j@gmail.com>
2024-11-01 08:49:18 +0300

    the basics of python from lists,sets,tuples,dictionaries,

---
+++ .venv/lesson8.py
@@ -0,0 +1,14 @@
+#dictionary
+#list[] ,tuple() ,dictionary{}
+# variable={"key":"value","key":"value","key":"value" }
+student={"name":"Hannah Mburu","id":1234 ,"age": 21}
+print(student[ "name"])
+student["height"]= 5.4
+print(student)
+
+
+
+#set
+people={"john","Bill","JOHN","Hannah","Mercy"}
+print(people)
+print(len(people))
